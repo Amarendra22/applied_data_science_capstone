5617b89644f57d27fb449f8911b20a84127324e0
Amarendra22 <120000271+Amarendra22@users.noreply.github.com>
2023-07-28 22:21:11 +0530

    Add files via upload

---
+++ dash_interactivity.py
@@ -0,0 +1,29 @@
+python3.8 -m pip install pandas dash
+wget "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DS0321EN-SkillsNetwork/datasets/spacex_launch_dash.csv"
+wget "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DS0321EN-SkillsNetwork/labs/module_3/spacex_dash_app.py"
+python3.8 spacex_dash_app.py
+dcc.Dropdown(id='id',
+                options=[
+                    {'label': 'All Sites', 'value': 'ALL'},
+                    {'label': 'site1', 'value': 'site1'},
+                ],
+                value='ALL',
+                placeholder="place holder here",
+                searchable=True
+                ),
+@app.callback(Output(component_id='success-pie-chart', component_property='figure'),
+              Input(component_id='site-dropdown', component_property='value'))
+def get_pie_chart(entered_site):
+    filtered_df = spacex_df
+    if entered_site == 'ALL':
+        fig = px.pie(data, values='class', 
+        names='pie chart names', 
+        title='title')
+        return fig
+    else:
+dcc.RangeSlider(id='id',
+                min=0, max=10000, step=1000,
+                marks={0: '0',
+                       100: '100'},
+                value=[min_value, max_value])    
+                                    
